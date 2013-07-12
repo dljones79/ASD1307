ed3d02f87856574263d7453f471137df29e3cc5f
David Jones <dljones79@fullsail.edu>
2013-07-11 20:11:35 -0500

    JS Work
    Added pageinit's for home, form and display pages.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@
 // ASD 1307
 // ASD Work
 
+//pageinit for home page
+$('#home').on('pageinit', function(){
+	// code needed for home page goes here
+});
+
+//pageinit for form page
+$('#formPage').on('pageinit', function(){
+	// code needed for form page goes here
+});
+
+//pageinit for display page
+$('#display').on('pageinit', function(){
+	// code needed for display page goes here
+});

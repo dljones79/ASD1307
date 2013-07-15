a43adb5026a8429e2eef362eed76d88f3c152c2c
David Jones <dljones79@fullsail.edu>
2013-07-15 17:19:12 -0500

    Beginning work on JS
    Starting save function for form page.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,20 @@ $('#home').on('pageinit', function(){
 //pageinit for form page
 $('#formPage').on('pageinit', function(){
 	// code needed for form page goes here
+	var storeData = function(data){
+		if(!data){
+			var uniqueId = Math.floor(Math.random()*1000000001);
+		}else{
+			var uniqueId = data;
+		}// end if 
+		
+		var obj ={};
+			obj.sName = ["School:", $('sName').value];
+			obj.contact = ["Contact:", $('contact').value];
+			obj.cNumber = ["Phone #:", $('cNUmber').value];
+			obj.building = ["Building:", $('building').value];
+			
+	};// end storeData
 });
 
 //pageinit for display page
